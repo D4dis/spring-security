c988127191c87e482a6e17e8de77877198c925bc
Dadis <raditztv@gmail.com>
2025-08-27 14:49:16 +0200

    tp1-b

--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,12 +17,37 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .httpBasic(Customizer.withDefaults())
+//                .httpBasic(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/hello-public").permitAll()
+                        .requestMatchers("/showuser").permitAll()
                         .requestMatchers("/hello-private").authenticated()
                 )
-                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(form -> form.loginPage("/login")
+                        .defaultSuccessUrl("/hello-private", true)
+                        .permitAll()
+                        .failureUrl("/hello-public")
+                )
                 .build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(PasswordEncoder passwordEncoder) {
+        UserDetails user1 = User.withUsername("user1")
+                .password(passwordEncoder.encode("pwd1"))
+                .roles("USER")
+                .build();
+        UserDetails user2 = User.withUsername("user2")
+
+                .password(passwordEncoder.encode("pwd2"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user1, user2);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
---
+++ src/main/java/com/example/demo/UserController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UserController {
+    @GetMapping("/showuser")
+    @ResponseBody
+    public String showUser(Authentication authentication) {
+        return "User connected : " + authentication.getName();
+    }
+}
